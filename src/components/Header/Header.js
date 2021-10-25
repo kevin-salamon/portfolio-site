c9897c982c1f5bd7a66db31973d5523513efe711
kevin-salamon <kevinwebdevs@gmail.com>
2021-10-25 09:28:30 -0400

    header detail

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Download } from 'react-feather';
 
 const StyledHeader = styled.div`
     height: 80px;
@@ -9,12 +10,106 @@ const StyledHeader = styled.div`
     left: 0;
     top: 0;
     z-index: 1;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+
+    .navigation-container {
+        width: min-content;
+        height: 70%;
+        margin-right: 10px;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+
+        .nav-box {
+            width: 120px;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: #480355;
+            margin: 0px 10px 0px 10px;
+
+            p {
+                margin-block: 0px;
+            }
+
+            .download {
+                width: 90%;
+                height: 80%;
+                border-radius: 8px;
+                border: 2px solid #B2FFD6;
+                cursor: pointer;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+
+                .text {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    height: 100%;
+                    width: 70%;
+                    padding-left: 2px;
+
+                    p {
+                        color: #B2FFD6;
+                        font-family: 'Space Mono', monospace;
+                    }
+                }
+
+                .icon {
+                    display: flex;
+                    justify-content: center;
+                    padding-right: 5px;
+                    align-items: center;
+                    height: 100%;
+                    width: 30%;
+
+                    svg {
+                        height: 20px;
+                        width: 20px;
+                        stroke: #B2FFD6;
+                        stroke-width: 2;
+                        stroke-linecap: round;
+                        stroke-linejoin: round;
+                        fill: none;
+                    }
+                }
+            }
+        }
+    }
 `;
 
 function Header() {
+
     return (
         <StyledHeader>
-
+            <div className='navigation-container'>
+                <div className='nav-box'>
+                    <div className='download'>
+                        <div className='text'>
+                            <p>Resume</p>
+                        </div>
+                        <div className='icon'>
+                            <Download />
+                        </div>
+                    </div>
+                </div>
+                <div className='nav-box'>
+                    
+                </div>
+                <div className='nav-box'>
+                
+                </div>
+                <div className='nav-box'>
+                
+                </div>
+                <div className='nav-box'>
+                
+                </div>
+            </div>
         </StyledHeader>
     );
 }
